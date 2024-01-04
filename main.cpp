cbc101ca28448a36afd100a00cb94d4f1105926b
FBSelosa <154306497+DENN-SELOSA020@users.noreply.github.com>
2024-01-04 12:52:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+int MainMenu();
+int InputData();
+int Addition(int, int);
+int Subtract(int, int);
+int Multiply(int, int);
+int Division(int, int);
+
 int main()
 {
     int val1, val2, choice;
@@ -7,27 +14,18 @@ int main()
 
     do
     {
+    	int MainMenu();
         do
         {
-            std::cout
-                << "--- Please Select for Math Solution to use ---\n"
-                << "1. Add\n"
-                << "2. Subtract\n"
-                << "3. Multipliction\n"
-                << "4. Division\n"
-                << "\n"
-                << "Enter Choice: ";
-            std::cin >> choice;
-
-            system("cls");
-
+            std::cout << "Please input value 1: ";
+            std::cin >> val1;
+            std::cout << "Please input value 2: ";
+            std::cin >> val2;
+            
             switch (choice)
             {
             case 1:
-                std::cout << "Please input value 1: ";
-                std::cin >> val1;
-                std::cout << "Please input value 2: ";
-                std::cin >> val2;
+            	int Addition(int val1, int val2);
                 std::cout << val1 << " + " << val2 << " = " << val1 + val2 << "\n";
                 break;
             case 2:
@@ -63,4 +61,39 @@ int main()
         system("cls");
 
     } while (continueChoice == 'y');
+}
+
+int MainMenu(){
+	int choice;
+	 std::cout
+                << "--- Please Select for Math Solution to use ---\n"
+                << "1. Add\n"
+                << "2. Subtract\n"
+                << "3. Multipliction\n"
+                << "4. Division\n"
+                << "\n"
+                << "Enter Choice: ";
+            std::cin >> choice;
+            
+            system("cls");
+            
+    return choice;
+}
+
+int InputData(int no, int& val){
+	std::cout << "Input value: ";
+	std::cin >> val;
+	return val;
+}
+int Addition(int x, int y){
+	return x + y;
+}
+int Subtract(int x, int y){
+	return x - y;
+}
+int Multiply(int x, int y){
+	return x * y;
+}
+int Division(int x, int y){
+	return x / y;
 }
